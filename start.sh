c011b47b06b4848eafda0d6b606e170ca711d5d3
mrsammy07 <66665117+mrsammy07@users.noreply.github.com>
2021-09-20 22:05:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b alpha https://github.com/TeamDeeCode/VcVideoPlayer.git /VcVideoPlayer
-cd /VcVideoPlayer
+git clone -b alpha https://github.com/mrsammy07/VcVideoPlay.git /VcVideoPlay
+cd /VcVideoPlay
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
