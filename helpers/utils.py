8cbfcfa76d9a0719253aaac70ac7f5a8c8b1faa9
mrsammy07 <66665117+mrsammy07@users.noreply.github.com>
2021-09-22 19:15:39 +0530

    Update utils.py

--- helpers/utils.py
+++ helpers/utils.py
@@ -526,7 +526,7 @@ async def is_admin(_, client, message: Message):
 
 async def get_playlist_str():
     if not Config.playlist:
-        pl = f"🎉 **Yooi Streaming [Startup Stream]({Config.STREAM_URL}) ▶️ !**"
+        pl = f"🎉 **Yo Streaming [Startup Stream]({Config.STREAM_URL}) !**"
     else:
         if len(Config.playlist)>=25:
             tplaylist=Config.playlist[:25]
